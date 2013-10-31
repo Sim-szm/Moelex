8529acf6a02f1987e14eeda6e988d1853c8d3278
Sim-szm <xianszm007@gmail.com>
2013-10-31 16:05:38 +0800

    MM

---
+++ src/memory-manage/moe-pmm.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  moe-pmm.c
+ *        Version:  1.0
+ *        Created:  2013年10月31日 15时37分02秒
+ *       Revision:  none
+ *       Compiler:  clang
+ *         Author:  sim szm, xianszm007@gmail.com
+ *        Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+#include "moe-pmm.h"
+uint32_t pmm_stack_loc=PMM_STACK_ADDR; //start location for alloc memory
+uint32_t pmm_stack_max=PMM_STACK_ADDR; //max location for alloc mempry
+uint32_t pmm_location; // no paging 
+char mm_paging_active=0; //flag for paging manage
+void init_pmm(multiboot *mboot_ptr){
+
+}
