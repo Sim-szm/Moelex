46d80dede0610d3f4613e34eaf5762892e597cdd
Sim-szm <xianszm007@gmail.com>
2013-11-01 10:11:57 +0800

    format_h

--- src/include/moe-descriptor_tables.h
+++ src/include/moe-descriptor_tables.h
@@ -16,8 +16,7 @@
 #include "moe-common.h"
 #include "moe-monitor.h"
 #include "moe-string.h"
-struct gdt_entry_struct
-{
+struct gdt_entry_struct{
    uint16_t limit_low;           // The lower 16 bits of the limit.
    uint16_t base_low;            // The lower 16 bits of the base.
    uint8_t  base_middle;         // The next 8 bits of the base.
@@ -37,8 +36,7 @@ void init_descriptor_tables();
 // GDT 加载到 GDTR 的函数[汇编实现]
 extern void gdt_flush(uint32_t);
 // A struct describing an interrupt gate.
-struct idt_entry_struct
-{
+struct idt_entry_struct{
    uint16_t base_lo;             // The lower 16 bits of the address to jump to when this interrupt fires.
    uint16_t sel;                 // Kernel segment selector.
    uint8_t  always0;             // This must always be zero.
@@ -49,8 +47,7 @@ typedef struct idt_entry_struct idt_entry_t;
 
 // A struct describing a pointer to an array of interrupt handlers.
 // This is in a format suitable for giving to 'lidt'.
-struct idt_ptr_struct
-{
+struct idt_ptr_struct{
    uint16_t limit;
    uint32_t base;                // The address of the first element in our idt_entry_t array.
 } __attribute__((packed));
