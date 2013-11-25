928c0196fd253bde267b1c32040d39a571cdd2e1
Sim-szm <xianszm007@gmail.com>
2013-11-25 17:40:33 +0800

    multi_thread stuff

---
+++ src/kthread/scheduler.c
@@ -0,0 +1,65 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  scheduler.c
+ *        Version:  1.0
+ *        Created:  2013年11月25日 17时19分38秒
+ *       Revision:  none
+ *       Compiler:  clang
+ *         Author:  sim szm, xianszm007@gmail.com
+ *        Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+#include "heap_manage.h"
+#include "kthread.h"
+#include "scheduler.h"
+void scheduler(){
+	if(!ready_queue)
+	      return ;
+	thread_list_t *iterator=ready_queue;
+	while(iterator->next){
+		iterator=iterator->next;
+	}
+	iterator->next=current_thread;
+	current_thread=ready_queue;
+	ready_queue=ready_queue->next;
+	switch_thread_work(current_thread->thread);
+}
+void init_scheduler(thread_t *initial_thread){
+	current_thread=(thread_list_t*)kmalloc(sizeof(thread_list_t*));
+	current_thread->thread=initial_thread;
+	current_thread->next=0;
+	ready_queue=0;
+}
+void thread_is_ready(thread_t *t){
+	thread_list_t *item=(thread_list_t*)kmalloc(sizeof(thread_list_t*));
+	item->thread=t;
+	item->next=0;
+	if(!ready_queue)
+	      ready_queue=item;
+	else{
+		thread_list_t *iterator=ready_queue;
+		while(iterator->next){
+			iterator=iterator->next;
+		}
+		iterator->next=item;
+	}
+}
+void thread_not_ready(thread_t *t){
+	// Attempt to find the thread in the ready queue.
+	thread_list_t *iterator=ready_queue;
+	if(iterator->thread==t){
+		ready_queue=iterator->next;
+		kfree(iterator);
+		return ;
+	}
+	while(iterator->next){
+		if(iterator->next->thread==t){
+			thread_list_t *loca=iterator->next;
+			iterator->next=loca->next;
+			kfree(loca);
+		}
+		iterator=iterator->next;
+	}
+}
---
+++ src/kthread/thread.c
@@ -0,0 +1,47 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  thread.c
+ *        Version:  1.0
+ *        Created:  2013年11月25日 16时31分04秒
+ *       Revision:  none
+ *       Compiler:  clang
+ *         Author:  sim szm, xianszm007@gmail.com
+ *        Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+#include "heap_manage.h"
+#include "kthread.h"
+#include "string.h"
+thread_t *init_threading(){
+	thread_t *thread=kmalloc(sizeof(thread_t));
+	thread->id=next_tid++;
+	current_thread=thread;
+	return thread;
+}
+thread_t *create_thread(int(*fn)(void*),void*arg,uint32_t *stack){
+	thread_t *thread=kmalloc(sizeof(thread_t));
+	memset(thread,0,sizeof(thread_t));
+	thread_t->id=next_tid++;
+	*--stack=(uint32_t)arg;
+	*--stack=(uint32_t)&thread_exit;
+	*--stack=(uint32_t)fn;
+	*--stack=0;
+	thread->ebp=(uint32_t)stack;
+	thread->esp=(uint32_t)stack;
+	return thread;
+}
+void switch_thread_work(thread_t *next){
+	asm volatile("mov %%esp ,%0":"=r" (current_thread->esp));
+	asm volatile("mov %%ebp ,%0":"=r" (current_thread->ebp));
+	asm volatile("mov %%ebx ,%0":"=r" (current_thread->ebx));
+	asm volatile("mov %%esi ,%0":"=r" (current_thread->esi));
+	asm volatile("mov %%edi ,%0":"=r" (current_thread->edi));
+	current_thread=next;
+	asm volatile("mov %%edi ,%0":"=r" (current_thread->edi));
+	asm volatile("mov %%esi ,%0":"=r" (current_thread->esi));
+	asm volatile("mov %%ebx ,%0":"=r" (current_thread->ebx));
+	asm volatile("mov %%esp ,%0":"=r" (current_thread->esp));
+	asm volatile("mov %%ebp ,%0":"=r" (current_thread->ebp));
+}
